b1ed6217cfac6372838a0c21a5d48a606c2a66eb
Carlos <jcflobe@gmail.com>
2020-12-20 17:43:03 -0700

    fetching all first generation pokemons

--- src/script.js
+++ src/script.js
@@ -1,11 +1,42 @@
-const poke_container = document.getElementById('poke_container');
-const pokemons_number = 150;
+document.addEventListener('DOMContentLoaded', () => {
 
-const getPokemon = async id => {
-    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
-    const res = await fetch(url);
-    const pokemon = await res.json();
-    console.log(pokemon);
-}
+    const poke_container = document.getElementById('poke_container');
+    const pokemons_number = 150;
 
-getPokemon(1)
+    
+
+    const fetchPokemons = async () => {
+        for (let i = 1; i <= pokemons_number; i++) {
+            await getPokemon(i);
+        }
+    }
+
+    const getPokemon = async id => {
+        const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+        const res = await fetch(url);
+        const pokemon = await res.json();
+        createPokemonCard(pokemon);
+    }
+
+    fetchPokemons();
+
+    function createPokemonCard(pokemon){
+        const pokemonEl = document.createElement('div');
+        pokemonEl.classList.add('pokemon');
+        const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+        const pokeInnerHTML = `
+        <div class="img-container">
+        <img src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png">
+        </div>
+        ${name}
+        
+        `;
+
+        pokemonEl.innerHTML = pokeInnerHTML;
+
+        poke_container.appendChild(pokemonEl);
+
+    }
+
+
+})
